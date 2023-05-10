32cb7ce90378b3ba2379178e77861f359d84a6cf
Пидор Не <strangeman21@outlook.com>
2023-05-10 10:47:47 +0500

    Фикс статического бага

---
+++ cpp.Wallet/ImplAccount.cpp
@@ -0,0 +1,48 @@
+#include "pch.h"
+#include "Account.h"
+
+class Account::impl 
+{
+private:
+	static int32_t m_accountId;
+	int32_t m_currnetId;
+	std::string m_name = "test";
+	Wallet m_wallet = Wallet::DOLLAR;
+	int64_t m_value = 0;
+public:
+	impl(std::string name, Wallet wallet, int64_t value) :
+		m_name(name), m_wallet(wallet), m_value(value)
+	{
+		m_currnetId = ++m_accountId;
+	}
+	~impl()	{ }
+public:
+	void setName(std::string name) { m_name = name; }
+	std::string getName() { return m_name; }
+
+	int32_t getAccountId() { return m_currnetId; }
+	Wallet getWallet() { return m_wallet; }
+	int64_t getValue() { return m_value; }
+
+	void addValue(int32_t value) { m_value += value; }
+	void reduceValue(int32_t value) { m_value -= value; }
+};
+
+int32_t Account::impl::m_accountId = 0;
+
+Account::Account(std::string name, Wallet wallet, int64_t value) :
+	pimpl(std::make_unique<impl>(name, wallet, value))
+{ }
+
+Account::~Account() 
+{ }
+
+void Account::setName(std::string name) { pimpl->setName(name); }
+std::string Account::getName() { return pimpl->getName(); }
+
+int32_t Account::getAccountId() { return pimpl->getAccountId(); }
+Wallet Account::getWallet() { return pimpl->getWallet(); }
+int64_t Account::getValue() { return pimpl->getValue(); }
+
+void Account::addValue(int32_t value) { pimpl->addValue(value); }
+void Account::reduceValue(int32_t value) { pimpl->reduceValue(value); }
---
+++ testConsole/testConsole.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include "Account.h"
+
+int main()
+{
+    Account acc("testo", Wallet::DOLLAR, 21);
+    Account acc1("test1o", Wallet::DOLLAR, 21);
+
+    std::cout << acc.getAccountId() << " " << acc1.getAccountId();
+    std::cin.get();
+    std::cin.get();
+}
