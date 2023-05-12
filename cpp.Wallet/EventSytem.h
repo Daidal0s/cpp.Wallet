e4fccfaf21b29289b2e0e09d12b4705f0fba5e07
StrvngeMan21 <strangeman21@outlook.com>
2023-05-12 15:29:04 +0500

    nkm ,

--- cpp.Wallet/EventSytem.h
+++ cpp.Wallet/EventSytem.h
@@ -3,44 +3,31 @@
 
 namespace eventSystem
 {
-	class MYDLL_API Subscriber
+	class Publisher
 	{
-		friend class Publisher;
 	private:
 		class impl;
 		std::unique_ptr<impl> pimpl;
 	public:
-		Subscriber(Publisher &pub);
-		~Subscriber();
+
+	};
+
+	class MYDLL_API Subscriber : public ISubscriber
+	{
+	private:
+		class impl;
+		std::unique_ptr<impl> pimpl;
 	public:
-		Subscriber(const Subscriber& other);
-		Subscriber& operator=(const Subscriber& rhs);
-		Subscriber(Subscriber&& other) noexcept;
-		Subscriber& operator=(Subscriber&& rhs) noexcept;
-	public:
-		void update(const std::string& messageByPub);
-		int32_t getId() const;
+		void update(/**/);
 	};
 
 	class MYDLL_API Publisher
 	{
-		friend class Subscriber;
 	private:
 		class impl;
 		std::unique_ptr<impl> pimpl;
 	public:
-		Publisher(const std::string& name);
-		~Publisher();
-	public:
-		Publisher(const Publisher& other);
-		Publisher& operator=(const Publisher& rhs);
-		Publisher(Publisher&& other) noexcept;
-		Publisher& operator=(Publisher&& rhs) noexcept;
-	public:
-		void addSub(Subscriber *sub);
-		void removeSub(Subscriber *sub);
-		void updateState(const std::string &str);
-		void notify();
+
 	};
 
 }
--- cpp.Wallet/EventSytemImpl.cpp
+++ cpp.Wallet/EventSytemImpl.cpp
@@ -28,35 +28,6 @@ public:
 	int32_t getId() const { return m_currentId; }
 };
 
-int32_t Subscriber::impl::m_pubId = 0;
-
-Subscriber::Subscriber(Publisher& pub) :
-	pimpl(std::make_unique<impl>(pub))
-{ }
-Subscriber::Subscriber(const Subscriber& other) :
-	pimpl(std::make_unique<impl>(other))
-{ }
-Subscriber& Subscriber::operator=(const Subscriber& rhs)
-{
-	if (this != &rhs)
-		pimpl.reset(new impl(*rhs.pimpl));
-	return *this;
-}
-Subscriber::Subscriber(Subscriber&& other) noexcept :
-	pimpl(std::move(other.pimpl))
-{ }
-Subscriber& Subscriber::operator=(Subscriber&& rhs) noexcept
-{
-	if (this != &rhs)
-		pimpl = std::move(rhs.pimpl);
-	return *this;
-}
-
-Subscriber::~Subscriber() { }
-
-void Subscriber::update(const std::string& messageByPub) { pimpl->update(messageByPub); }
-int32_t Subscriber::getId() const { return pimpl->getId(); }
-
 
 //////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -84,32 +55,3 @@ public:
 	void notify() { for (auto c : m_subs) c->update(m_name); }
 
 };
-
-Publisher::Publisher(const std::string& name) :
-	pimpl(std::make_unique<impl>(name))
-{ }
-Publisher::~Publisher() { }
-
-Publisher::Publisher(const Publisher& other) :
-	pimpl(std::make_unique<impl>(other))
-{ }
-Publisher& Publisher::operator=(const Publisher& rhs)
-{
-	if (this != &rhs)
-		pimpl.reset(new impl(*rhs.pimpl));
-	return *this;
-}
-Publisher::Publisher(Publisher&& other) noexcept :
-	pimpl(std::move(other.pimpl))
-{ }
-Publisher& Publisher::operator=(Publisher&& rhs) noexcept
-{
-	if (this != &rhs)
-		pimpl = std::move(rhs.pimpl);
-	return *this;
-}
-
-void Publisher::addSub(Subscriber* sub) { pimpl->addSub(sub); }
-void Publisher::removeSub(Subscriber* sub) { pimpl->removeSub(sub); }
-void Publisher::updateState(const std::string& str) { pimpl->updateState(str); }
-void Publisher::notify() { pimpl->notify(); }
