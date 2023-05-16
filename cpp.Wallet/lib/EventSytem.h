7708674a5755f87146755f1b107da22648a20d9c
StrvngeMan21 <strangeman21@outlook.com>
2023-05-16 13:29:11 +0500

    Почти понятая система событий без имплементации из-за шаблона

--- cpp.Wallet/lib/EventSytem.h
+++ cpp.Wallet/lib/EventSytem.h
@@ -3,31 +3,105 @@
 
 namespace eventSystem
 {
-	class Publisher
+	enum class NotifyAction { Done, UnRegister };
+
+	class Subject;
+	class Event
+	{
+	};
+
+	class Observer
+	{
+	public:
+		virtual ~Observer() {}
+		virtual NotifyAction onNotify(Subject& subject, Event const& event) = 0;
+	};
+
+	class Subject
 	{
 	private:
-		class impl;
-		std::unique_ptr<impl> pimpl;
+		std::vector<Observer*> m_observers;
 	public:
+		void registerObserver(Observer& observer)
+		{
+			if (std::find(std::begin(m_observers), std::end(m_observers), &observer) == std::end(m_observers))					// ВТФ? Должен ведь находить и возвращать first, а не last
+			{
+				throw std::runtime_error("registerObserver(): observer already registered");
+			}
+			m_observers.push_back(&observer);
+		}
+
+		void unregisterObserver(Observer& observer)																				// Очистка от смотретелей
+		{
+			m_observers.erase(std::remove(std::begin(m_observers), std::end(m_observers), &observer), std::end(m_observers));
+		}
+
+		void notifyObservers(Event const& event)																				// Уведомление смотртелей
+		{
+			std::vector<Observer*> deadObservers;
+			for (Observer* observer : m_observers)
+			{
+				if (observer->onNotify(*this, event) == NotifyAction::UnRegister)
+				{
+					deadObservers.push_back(observer);
+				}
+			}
+
+			auto newEnd = std::end(m_observers);
+			for (Observer* dead : deadObservers)
+			{
+				newEnd = std::remove(std::begin(m_observers), newEnd, dead);
+			}
+			m_observers.erase(newEnd, std::end(m_observers));
+		}
+	};
+
+	class EventHandler : public Observer
+	{
+	private:
+		std::unordered_map<std::type_index, std::function<void(Subject&, Event const&)>> m_handlers;
+	public:
+		NotifyAction onNotify(Subject& subject, Event const& event) override													// Уведомить нормально смотртелей
+		{
+			auto find = m_handlers.find(std::type_index(typeid(event)));
+			if (find != m_handlers.end()) {
+				find->second(subject, event);
+			}
+			return NotifyAction::Done;
+		}
+
+		template<typename T>
+		void registerEventHandler(std::function<void(Subject&, Event const&)> handler)
+		{
+			m_handlers[std::type_index(typeid(T))] = handler;																	// Оборачивает тип функции для создания ключа события и самого события, включая ссылка на функцию в карту
+		}
 
 	};
 
-	class MYDLL_API Subscriber : public ISubscriber
-	{
-	private:
-		class impl;
-		std::unique_ptr<impl> pimpl;
-	public:
-		void update(/**/);
-	};
+	// ПРИМЕР СОЗДАНИЯ ОБРАБОТЧИКОВ СОБЫТИЙ
 
-	class MYDLL_API Publisher
-	{
-	private:
-		class impl;
-		std::unique_ptr<impl> pimpl;
-	public:
+	////	class MouseMove : public Event {};
+	////	class MouseClick : public Event {};
 
-	};
+	////	class MouseHandler : public EventHandler
+	////	{
+	////	private:
+	////		void mouseClick(Subject&, Event const&)
+	////		{
+	////			std::cout << "Mouse Clicked\n";
+	////		}
+	////	public:
+	////		MouseHandler()
+	////		{
+	////			// Register a call to a member method
+	////			registerEventHandler<MouseClick>(std::bind(&MouseHandler::mouseClick, this, std::placeholders::_1, std::placeholders::_2));		// Регистрит событие нажатия мышки, создавая через bind делегат на функцию mouseClick,
+	////																																			// Таким образом передаёт ссылку на функцию 
+	////			/*MouseHandler    mouseEventHandler;
+	////			mouseEventHandler.registerEventHandler<MouseMove>([](Subject&, Event const&) {std::cout << "Mouse Moved\n"; });*/				// Лямбда
+	////		}
+	////	};
 
+	////	class Mouse : public Subject
+	////	{
+	////	};
 }
--- cpp.Wallet/lib/pch.h
+++ cpp.Wallet/lib/pch.h
@@ -9,11 +9,17 @@
 
 // Добавьте сюда заголовочные файлы для предварительной компиляции
 #include "framework.h"
-#include <memory>
-#include <string>
-#include <cstdint>
-#include <list>
-#include <chrono>
-#include <functional>
+#include <iostream>						// Хэдер ввода и вывода
+#include <cstdint>						// Определение int32_t и т.д.
+#include <chrono>						// Время со стандартом C++20
+#include <vector>						// Динамический массив
+#include <unordered_map>				// Хэш-мап
+#include <list>							// Лист
+#include <string>						// Для убоства использования строковых типов
+#include <algorithm>					// std::last, std::first, std::placeholders::_1
+#include <memory>						// std::unique_ptr<>		-Умный указатель
+#include <functional>					// std::function			-Указатель на функцию (делегат)
+#include <typeindex>					// 
+#include <stdexcept>					// Для обработки ошибок
 
 #endif //PCH_H
